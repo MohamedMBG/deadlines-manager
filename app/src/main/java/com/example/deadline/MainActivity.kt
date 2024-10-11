3251d5db0dfa9d435599e8089d405f638b879b6a
MohamedMBG <mohamedbaghdad.me@gmail.com>
2024-10-11 13:58:48 +0100

    this is the start of the project

---
+++ app/src/main/java/com/example/deadline/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.deadline
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        /*
+            this android project is gonna be a deadline + time management app at the same time
+            using firebase to allow users to create their own data and authenticate on other devices as well
+        */
+
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
